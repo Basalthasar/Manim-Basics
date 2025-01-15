22a56d7e11cbfb77a1a3c07904aa9a0870a5455e
Basalthasar <balthasar@przyklenk.org>
2025-01-15 22:52:38 +0100

    Best. einer Abl.: Sekante und Näherung 1

--- Code/Analysis/Extremwertbestimmung/Bestimmung einer Ableitung.py
+++ Code/Analysis/Extremwertbestimmung/Bestimmung einer Ableitung.py
@@ -27,7 +27,7 @@ class Ableitung(Scene):
         Graph_ganz = VGroup(Achsen, Achsenbeschriftung, Graph, Funktionsterm)
         Graph_ganz.scale(0.95).shift(DOWN*0.75)
 
-        # Einfaden der Achsen, Achsenbeschriftung und des Graphen
+        # Achsen und Graph
         self.play(Create(Achsen))
         self.wait(0.25)
         self.play(Write(Achsenbeschriftung))
@@ -38,10 +38,10 @@ class Ableitung(Scene):
         self.play(Graph_ganz.animate.shift(LEFT * 2).scale(0.8), run_time=2)
         self.wait(2)
 
-        # 1. Durchschnittliche Steigung beschreiben
+        # Punkte auf dem Graphen
         Stichpunkt1 = MathTex(
             r"\text{Zwei Punkte auf}",
-            r"\\", # Für einen Zeilenumbruch
+            r"\\",
             r"\text{der Funktion:}",
             r"\\",
             r"p_1", r"\text{ und }", r"p_2"
@@ -52,7 +52,6 @@ class Ableitung(Scene):
 
         self.play(Write(Stichpunkt1), run_time=1)
 
-        # Punkte auf dem Graphen
         p1 = Dot(Achsen.c2p(0, 0), color=PURE_RED)
         p2 = Dot(Achsen.c2p(0, 0), color=PURE_RED)
         p1_label = MathTex("p_1", color=PURE_RED).next_to(p1, DOWN)
@@ -60,14 +59,12 @@ class Ableitung(Scene):
         self.play(FadeIn(p1, p1_label))
         self.play(FadeIn(p2))
 
-        # Pfad für p2 entlang des Graphen
+        # p2 entlang des Graphen bewegen
         origin = Achsen.c2p(0, 0)
         graph_end = Graph.point_from_proportion((1.5 - Graph.t_min) / (Graph.t_max - Graph.t_min))
 
-        # Pfad entlang des Graphen von 0 bis 1.5
         path_along_graph = Graph.get_subcurve((0 - Graph.t_min) / (Graph.t_max - Graph.t_min), (1.5 - Graph.t_min) / (Graph.t_max - Graph.t_min))
 
-        # Kombinierter Pfad
         combined_path = VMobject()
         combined_path.set_points_as_corners([origin])
         combined_path.append_points(path_along_graph.get_points())
@@ -81,18 +78,73 @@ class Ableitung(Scene):
         vert_line = DashedLine(start=p2.get_bottom(), end=Achsen.c2p(1.5, 0), color=WHITE)
         self.play(Create(vert_line))
 
-        # Gerade durch p1 und p2, verlängert
+        # Sekantenlinie zwischen p1 und p2
         secant_line = Line(
             start=p1.get_center(), 
             end=p2.get_center(), 
             color=RED
         )
-        secant_line.scale(1.25, about_point=secant_line.get_start())
-        secant_line.scale(1.25, about_point=secant_line.get_end())
-        secant_line.set_z_index(-3)  # Setzt den z-index auf einen niedrigeren Wert, um die Sekante hinter den Graphen zu legen
+        midpoint = (p1.get_center() + p2.get_center()) / 2
+        direction = p2.get_center() - p1.get_center()
+        direction /= np.linalg.norm(direction)
+        fixed_length = 8
+        secant_line.put_start_and_end_on(midpoint - direction * (fixed_length / 2), midpoint + direction * (fixed_length / 2))
+        secant_line.set_z_index(-3)
 
         self.play(Create(secant_line))
 
-        self.play(Stichpunkt1.animate.scale(0.75).shift(UP*1.5), run_time=1)
+        self.play(Stichpunkt1.animate.scale(0.75).shift(UP*1.75), run_time=1)
 
         self.wait(2)
+
+        # Zweiter Punkt nähert sich dem ersten Punkt
+        Stichpunkt2 = MathTex(
+            r"p_1", r"\text{ und }", r"p_2", r"\text{ sich}",
+            r"\\",
+            r"\text{annähern lassen}"
+        )
+        Stichpunkt2[0].set_color(PURE_RED)
+        Stichpunkt2[2].set_color(PURE_RED)
+        
+        Stichpunkt2.next_to(Stichpunkt1, DOWN)
+        Stichpunkt2.scale(0.75)
+
+        self.play(Write(Stichpunkt2), run_time=1)
+        path_along_graph_p1 = Graph.get_subcurve((0 - Graph.t_min) / (Graph.t_max - Graph.t_min), (1.499999 - Graph.t_min) / (Graph.t_max - Graph.t_min))
+
+        combined_path_p1 = VMobject()
+        combined_path_p1.set_points_as_corners([origin])
+        combined_path_p1.append_points(path_along_graph_p1.get_points())
+
+        p1_label_path = combined_path_p1.copy()
+        p1_label_path.shift(DOWN * 0.5)
+
+        # Sekantenlinie aktualisieren, während sich p1 bewegt
+        def update_secant_line(line):
+            start = p1.get_center()
+            end = p2.get_center()
+            midpoint = (start + end) / 2
+            direction = end - start
+            direction /= np.linalg.norm(direction)
+            fixed_length = 8
+            line.put_start_and_end_on(midpoint - direction * (fixed_length / 2), midpoint + direction * (fixed_length / 2))
+
+        secant_line.add_updater(update_secant_line)
+
+        # Tangente bei p2
+        tangent_line = TangentLine(Graph, alpha=(1.5 - Graph.t_min) / (Graph.t_max - Graph.t_min), length=8, color=RED)
+
+        self.play(FadeOut(vert_line))
+        self.play(
+            MoveAlongPath(p1, combined_path_p1), 
+            MoveAlongPath(p1_label, p1_label_path),
+            run_time=2
+        )
+
+        secant_line.remove_updater(update_secant_line)
+        
+        self.play(Transform(secant_line, tangent_line), run_time=2)
+
+        self.wait(2)
+
+        # TODO: Steigungsdreieck an Tangente einfügen, (f(x_0 + h) - f(x_0)) / h aufstellen
