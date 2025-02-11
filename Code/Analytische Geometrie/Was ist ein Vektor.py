46f7412c4a8b891bb38b0455a00a0596f1ae4768
Basalthasar <balthasar@przyklenk.org>
2025-02-11 20:11:57 +0100

    Analytische Geometrie 0.02.5: Anfang Implementation 3d Szene

--- Code/Analytische Geometrie/Was ist ein Vektor.py
+++ Code/Analytische Geometrie/Was ist ein Vektor.py
@@ -3,7 +3,7 @@ from manim import *
 config.frame_rate = 60
 config.max_files_cached = 500
 
-class Analytische_Geometrie_t1(Scene):
+class Analytische_Geometrie_t1(ThreeDScene):
 
     def Stichpunkt(self, content, size=18):
         # Stichpunkt und Inhalt
@@ -32,7 +32,7 @@ class Analytische_Geometrie_t1(Scene):
 
         self.wait(1)
 
-        Stichpunkte = VGroup(
+        Themen_Stichpunkte = VGroup(
             self.Stichpunkt("Definition eines Vektors"),
             self.Stichpunkt("Geradengleichungen"),
             self.Stichpunkt("Lagebeziehungen mit Geraden"),
@@ -46,18 +46,25 @@ class Analytische_Geometrie_t1(Scene):
             self.Stichpunkt("Geraden- und Ebenenscharen")
         ).arrange(DOWN, buff=0.25).shift(DOWN*0.8)
 
-        self.play(Write(Stichpunkte), run_time=7.5)
+        self.play(Write(Themen_Stichpunkte), run_time=7.5)
 
         self.wait(2)
 
         self.play(
             Titel.animate.shift(UP).set_opacity(0),
-            Stichpunkte[0].animate.move_to(UP*3).scale(4),
-            *[FadeOut(sp, shift=DOWN) for sp in Stichpunkte[1:]],
+            Themen_Stichpunkte[0].animate.move_to(UP*3).scale(4),
+            *[FadeOut(sp, shift=DOWN) for sp in Themen_Stichpunkte[1:]],
             run_time=1.5
         )
 
-        achsen = ThreeDAxes(
+        achsen = Axes(
+            x_range=[-2, 2, 1],
+            y_range=[-2, 2, 1],
+            x_length=4,
+            y_length=4,
+        ).shift(DOWN).scale(1.25)
+
+        achsen3d = ThreeDAxes(
             x_range=[-2, 2, 1],
             y_range=[-2, 2, 1],
             z_range=[-2, 2, 1],
@@ -67,51 +74,62 @@ class Analytische_Geometrie_t1(Scene):
         ).shift(DOWN).scale(1.25)
 
         achsen.add_coordinates(font_size=25)
+        achsen3d.add_coordinates(font_size=25).set_z_index(-5)
 
         # Labels der Achsen
         x_label = achsen.get_x_axis_label(Tex("x"))
+        x_label3d = achsen3d.get_x_axis_label(Tex("x"))
         y_label = achsen.get_y_axis_label(Tex("y"))
-        z_label = achsen.get_z_axis_label(Tex("z"))
+        y_label3d = achsen3d.get_y_axis_label(Tex("y"))
+        z_label3d = achsen3d.get_z_axis_label(Tex("z"))
 
         # Vektoren
         vektor_x = Arrow3D(achsen.c2p(0, 0, 0), achsen.c2p(1, 0, 0), color=RED)
+        vektor_x3d = Arrow3D(achsen3d.c2p(0, 0, 0), achsen3d.c2p(1, 0, 0), color=RED)
         vektor_y = Arrow3D(achsen.c2p(1, 0, 0), achsen.c2p(1, 1, 0), color=GREEN)
+        vektor_y3d = Arrow3D(achsen3d.c2p(1, 0, 0), achsen3d.c2p(1, 1, 0), color=GREEN)
         vektor_z = Arrow3D(achsen.c2p(1, 1, 0), achsen.c2p(1, 1, 1), color=BLUE)
+        vektor_z3d = Arrow3D(achsen3d.c2p(1, 1, 0), achsen3d.c2p(1, 1, 1), color=BLUE)
 
         # Punkt
         p_1 = Dot3D(point=achsen.c2p(1, 1, 1), color=YELLOW)
+        p_13d = Dot3D(point=achsen3d.c2p(1, 1, 1), color=YELLOW)
         p_1_m = Dot3D(point=achsen.c2p(0, 0, 0), color=YELLOW_A).set_opacity(0)
+        p_1_m3d = Dot3D(point=achsen3d.c2p(1, 1, 0), color=YELLOW_A).set_opacity(0)
         p_1_beschriftung = MathTex("p_1", font_size=25, color=YELLOW).next_to(p_1, RIGHT, buff=0.1)
+        p_1_beschriftung3d = MathTex("p_1", font_size=25, color=YELLOW).next_to(p_13d, RIGHT, buff=0.1).rotate(PI/2, axis=RIGHT)
 
-        graph_ganz = VGroup(achsen, x_label, y_label, z_label, vektor_x, vektor_y, vektor_z, p_1, p_1_m, p_1_beschriftung)
+        graph_ganz = VGroup(achsen, x_label, y_label, vektor_x, vektor_y, vektor_z, p_1, p_1_m, p_1_beschriftung)
 
         # Animation
         self.play(
             Create(achsen),
             Write(x_label),
             Write(y_label),
-            Write(z_label),
             run_time=2
         )
 
         self.play(GrowFromPoint(vektor_x, achsen.c2p(0, 0, 0)))
         self.play(GrowFromPoint(vektor_y, achsen.c2p(1, 0, 0)))
+        vektor_z.set_opacity(0) 
         self.play(GrowFromPoint(vektor_z, achsen.c2p(1, 1, 0)))
         self.play(Create(p_1), Write(p_1_beschriftung))
         self.wait(speed)
         self.play(graph_ganz.animate.move_to(LEFT * 4.25 + DOWN * 0.8), run_time=2)
 
         # Einführung: Text
-        Stichpunkt_1 = Tex("Um zum Punkt $p_1 (1|1)$ zu gelangen")
-        Stichpunkt_1_1 = BulletedList("muss man vom Ursprung 1 LE in die x-Richtung gehen", "und eine weitere LE in die y-Richtung").scale(0.65)
+        Stichpunkt_1 = Tex("Um zum Punkt $p_1 (1|1)$ zu gelangen\\\\ gilt in einem (2d) Koordinatensystem:").scale(0.95)
+        Stichpunkt_1_1 = BulletedList("Eine Längeneinheit in die x-Richtung gehen", "und eine weitere Längeneinheit in die y-Richtung").scale(0.65)
+        # Stichpunkt_2 = Tex("Erweitert man das Koordinatensystem\\\\um eine weitere Dimension...").scale(0.95)
+        # Stichpunkt_2_1 = BulletedList("eine weitere LE in die z-Richtung gehen").scale(0.65)
 
         # Positionierung
-        Stichpunkte = VGroup(Stichpunkt_1, Stichpunkt_1_1).arrange(DOWN, buff=0.25).next_to(achsen, RIGHT, buff=0.5).shift(UP*2.25)
+        Stichpunkte = VGroup(Stichpunkt_1, Stichpunkt_1_1).arrange(DOWN, buff=0.25, aligned_edge=LEFT).next_to(achsen, RIGHT, buff=0.5).shift(UP*1.25)
         Stichpunkt_1_1.shift(RIGHT * 0.25)
 
         # Colorcoding
         Stichpunkt_1[0][10:12].set_color(YELLOW)
-        Stichpunkt_1_1[0][19:37].set_color(RED)
+        Stichpunkt_1_1[0][1:500].set_color(RED)
         Stichpunkt_1_1[1][4:500].set_color(GREEN)
 
         vektor_x_path = ParametricFunction(
@@ -140,5 +158,36 @@ class Analytische_Geometrie_t1(Scene):
         self.wait(speed)
         self.play(Write(Stichpunkt_1_1[1]), MoveAlongPath(p_1_m, vektor_y_path), run_time=2)
         self.wait(speed)
+
+        self.wait(2)
+
+        self.play(
+            Unwrite(Stichpunkte),
+            Unwrite(Themen_Stichpunkte[0]),
+            run_time=1
+        )
+
+        self.move_camera(phi=45 * DEGREES, theta=45 * DEGREES, zoom=1.5, run_time=1.5)
+
+        self.play(
+            ReplacementTransform(p_1_beschriftung, p_1_beschriftung3d),
+            ReplacementTransform(x_label, x_label3d),
+            ReplacementTransform(y_label, y_label3d),
+            Write(z_label3d),
+            ReplacementTransform(achsen, achsen3d),
+            ReplacementTransform(vektor_x, vektor_x3d),
+            ReplacementTransform(vektor_y, vektor_y3d),
+            ReplacementTransform(p_1, p_13d),
+            ReplacementTransform(p_1_m, p_1_m3d),
+            Uncreate(vektor_z),
+            GrowFromPoint(vektor_z3d, achsen3d.c2p(1, 1, 0)),
+            run_time=3
+        )
+
+        self.play(
+            achsen.animate.shift(DOWN)
+        )
+
+        # holy shwabooms, das wird schwierig
 
         self.wait(2)
